a74220c2ae3eb8ee5a0722657865354a9646cbff
FelipeSPB <felipebiancalana@outlook.com>
2020-12-07 17:29:00 -0300

    Added icon by Freepik (https://www.flaticon.com/authors/freepik) and placed new settings on the app stage.

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +14,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Calculator by FelipeSPB");
+        Image image = new Image("/props/calculate.png");
+        primaryStage.getIcons().add(image);
         Scene scene =  new Scene(root, 450, 418);
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
