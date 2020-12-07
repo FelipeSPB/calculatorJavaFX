bbc96d8d86e354b16c957bdecb3aa660dceaa67e
FelipeSPB <felipebiancalana@outlook.com>
2020-12-06 21:43:17 -0300

    Correct the painel, allowing only one math symbol except the %.

--- src/application/Methods.java
+++ src/application/Methods.java
@@ -4,22 +4,24 @@ import java.util.ArrayList;
 
 public interface Methods {
 
-    static ArrayList settingMathArray(){
-        String [] symbols = {"+","-","x","÷"};
-        ArrayList<String> array = new ArrayList<String>();
-        for (String symbol: symbols) {
-            array.add(symbol);
-        }
-        return array;
-    }
 
     static boolean checkShowUser(String showUser) {
         if(showUser.length()>=10){
             return false;
         }
+        int counterForMathSymbols = 0;
+        for (int i = 0; i < showUser.length(); i++) {
+            String charIndex = Character.toString(showUser.charAt(i));
+            if (charIndex.equals("+") || charIndex.equals("-") || charIndex.equals("x") || charIndex.equals("÷")) {
+                counterForMathSymbols++;
+            }
+        }
+        if(counterForMathSymbols >= 1){
+            System.out.println(counterForMathSymbols);
+            return false;
+        }
         return true;
     }
-
-
+    
 
 }
